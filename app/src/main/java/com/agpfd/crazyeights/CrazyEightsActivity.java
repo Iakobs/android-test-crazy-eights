c6e7db1f7cfb26c0510223f8548cd18c5f5fad40
Jacob Ibáñez Sánchez <jacobmagno@gmail.com>
2016-07-16 17:29:10 +0200

    Primer commit
    Commit con los ficheros básicos del proyecto

---
+++ app/src/main/java/com/agpfd/crazyeights/CrazyEightsActivity.java
@@ -0,0 +1,13 @@
+package com.agpfd.crazyeights;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CrazyEightsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crazy_eights);
+    }
+}
